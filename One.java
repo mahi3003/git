1d030662db37274f4ab6c798e8cedf09ae3e0b94
MAHENDRAN <mahendran303096@gmail.com>
2025-06-08 17:36:53 +0530

    Task1

---
+++ One.java
@@ -0,0 +1,8 @@
+public class One {
+    public static void main(String[] args) {
+        System.out.println("Using for loop:");
+        for (int i = 10; i <= 50; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
